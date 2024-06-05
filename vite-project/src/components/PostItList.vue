e69d350e6ba693b80651f8aaa98c3ef98c4e4eb5
RACJHEL FANN <124321963+rae-2@users.noreply.github.com>
2024-06-05 13:13:04 -0400

    Update PostItList.vue

--- vite-project/src/components/PostItList.vue
+++ vite-project/src/components/PostItList.vue
@@ -1,6 +1,8 @@
 <template>
-    <div class="add">
+    <div>
+      <div class="add">
         <button class="add" @click="addPostIt">Add Post-it Note</button>
+      </div>
         <div class="post-it-container">
         <PostIt
             v-for="(note, index) in postIts"
