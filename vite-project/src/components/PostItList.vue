e2bc7a4bc1a500e1946250e62889f5bf3f4226ef
Seebso <124058253+Seebso@users.noreply.github.com>
2024-06-06 20:04:37 -0400

    yes1

--- vite-project/src/components/PostItList.vue
+++ vite-project/src/components/PostItList.vue
@@ -1,19 +1,17 @@
 <template>
-    <div>
-      <div class="add">
-        <button class="add" @click="addPostIt">Add Post-it Note</button>
+  <div>
+      <button @click="addPostIt" autofocus>Add Post-it Note</button>
+      <div class="post-it-container">
+      <PostIt
+          v-for="(note, index) in postIts"
+          :key="note.id"
+          :initialContent="note.content"
+        @update="updatePostIt(index, $event)"
+          @delete="handleDelete(index)"
+          
+        />
       </div>
-        <div class="post-it-container">
-        <PostIt
-            v-for="(note, index) in postIts"
-            :key="note.id"
-            :initialContent="note.content"
-          @update="updatePostIt(index, $event)"
-            @delete="handleDelete(index)"
-            
-          />
-        </div>
-    </div>
+  </div>
 </template>
 
 <script setup>
@@ -24,30 +22,31 @@ import PostIt from './PostIt.vue';
 import { supabase } from '@/supabase';
 const postIts = ref([]);
 const init = async()=>{
-  const { data, error } = await supabase.from("postits").select(); 
-  postIts.value = data;
+const { data, error } = await supabase.from("postits").select(); 
+postIts.value = data;
 }
 onMounted(init);
 const addPostIt = async() => {
-  const newNote = { id: uuidv4(), content: '' };
-  postIts.value.push(newNote);
-  const {error} = await supabase.from("postits").upsert(newNote)
+const newNote = { id: uuidv4(), content: '' };
+
+const {error} = await supabase.from("postits").upsert(newNote)
+if (!error){postIts.value.push(newNote);}
 }
 
 const updatePostIt = async(index, newContent) => {
-  postIts.value[index].content = newContent;
-  const {error} = await supabase.from("postits").update({content:newContent}).eq("id",postIts.value[index].id)
+postIts.value[index].content = newContent;
+const {error} = await supabase.from("postits").update({content:newContent}).eq("id",postIts.value[index].id)
 }
 
 const deletePostIt = async(index) => {
-  console.log("is it deleting:", index);
-  const {error} = await supabase.from("postits").delete().eq("id",postIts.value[index].id)
+console.log("is it deleting:", index);
+const {error} = await supabase.from("postits").delete().eq("id",postIts.value[index].id)
 
-  postIts.value.splice(index,1);
+postIts.value.splice(index,1);
 }
 
 const handleDelete = (index) => {
-  deletePostIt(index);
+deletePostIt(index);
 }
 
 /* const pls = () => {
@@ -56,25 +55,11 @@ postIts.value.newContent.splice(0,1); */
 
 </script>
 
-
 <style lang="scss" scoped>
 
 .post-it-container {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-  }
-
-.add {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    font-family: 'Manrope', sans-serif;
-    transition: all .5s;
-    color: rgb(233, 135, 151);
-  }
-
-.add:hover{
-  transform: scale(1.15);
+  display: flex;
+  flex-wrap: wrap;
 }
+
 </style>
--- vite-project/src/store.js
+++ vite-project/src/store.js
@@ -1,7 +1,26 @@
-import { reactive } from "vue";
-
-export const store = {
+/* import { reactive } from "vue"; */
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+/* export const store = {
   state: reactive({
     user: {},
   }),
-};
+}; */
+export const useUserStore = defineStore('user', {
+  /* let user = ref({})
+  function setUser(_user) {
+    user = _user
+  }
+  return { user, setUser }
+}) */
+state: () => {
+  return { user: {} }
+},
+// could also be defined as
+// state: () => ({ count: 0 })
+actions: {
+  setUser(_user) {
+    this.user = _user
+  },
+},
+})
